54875ef970e106ca0e82250a5ed8bcd07fa071d9
Tyler Bridger <tyler@192-168-20-150.tpgi.com.au>
2013-12-17 15:34:49 +0900

    Added CSS

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <html>
 	<head>
 		<title>Microblogging site</title>
+		<link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+		<link href='css/main.css' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
 		<div id="contentWrapper">

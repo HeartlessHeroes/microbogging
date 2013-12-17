10a8c2e61920b28c1364ccada056335c2ae8917b
Tyler <tylerbridger96@gmail.com>
2013-12-17 17:36:49 +0900

    fixed functions switch

--- functions.php
+++ functions.php
@@ -2,10 +2,10 @@
 
 function loadModule($switch){
 	switch ($switch){
-		case: 'profileImage'
+		case 'profileImage':
 			echo '<div id="profileImage" class="sidebarModule"><img src="https://pbs.twimg.com/profile_images/378800000281645191/148c9aa0fb90a3d0a422e28a66786db5.jpeg" class="image"></div>';
 			break;
-		case: 'followCount'
+		case 'followCount':
 			echo '<div id="followCount" class="sidebarModule">Followers: 100<br>Following: 200</div>';
 			break;
 	}

b1284c2b3e931563288599a002e442a8e06e252d
Tyler <tylerbridger96@gmail.com>
2013-12-17 17:20:44 +0900

    Removed test data

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+require 'functions.php';
+require 'sql.php';
+?>
 <html>
 	<head>
 		<title>Microblogging site</title>
@@ -9,13 +14,10 @@
 		</div>
 		<div id="contentWrapper">
 			<div id="sidebarWrapper">
-				<div class="sidebarModule profileImage"><img src="https://pbs.twimg.com/profile_images/378800000281645191/148c9aa0fb90a3d0a422e28a66786db5.jpeg"></div>
+				<div class="sidebarModule profileImage"><img src="https://pbs.twimg.com/profile_images/378800000281645191/148c9aa0fb90a3d0a422e28a66786db5.jpeg" class="image"></div>
 			</div>
 			<div id="timelineWrapper">
-				<div class="timelineEntry">This is a text post.</div>
-				<div class="timelineEntry">
-					<div class="imageWrapper"><img src="https://pbs.twimg.com/profile_images/378800000281645191/148c9aa0fb90a3d0a422e28a66786db5.jpeg" class="image"></div>
-				</div>
+				<?php timelinePosts(); ?>
 			</div>
 		</div>
 	</body>

29dd7637b6ca5f30f615e3b8eabc113d5d539ae2
Tyler <HeartlessSuperHeroes@gmail.com>
2013-12-17 20:38:36 +0900

    Added session checking to index.php

--- index.php
+++ index.php
@@ -1,43 +1,48 @@
 <?php
-session_start();
-$_SESSION['user'] = 'tyler';
-require 'functions.php';
-require 'sql.php';
+if(empty(session_id())){
+	session_start();
+	loginPage();
+} else {
+	require 'functions.php';
+	require 'sql.php';
+	?>
+	<html>
+		<head>
+			<title>Microblogging site</title>
+			<link href='http://fonts.googleapis.com/css?family=Signika:400,300,700,600' rel='stylesheet' type='text/css'>
+			<link href='css/main.css' rel='stylesheet' type='text/css'>
+		</head>
+		<body>
+			<div id="headerWrapper">
+			</div>
+			<div id="contentWrapper">
+				<div id="sidebarWrapper" class="curvedBorder">
+					<?php loadModule('profileImage'); ?>
+					<?php loadModule('followCount'); ?>
+					<?php loadModule('newPost'); ?>
+				</div>
+				<div id="timelineWrapper" class="curvedBorder">
+					<?php timelinePosts(NULL); ?>
+				</div>
+			</div>
+		</body>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+		<script>
+		$('textarea.newPost')
+		  .on('focus', function(){
+		      var $this = $(this);
+		      if($this.val() == 'Compose a new post...'){
+		          $this.val('');
+		      }
+		  })
+		  .on('blur', function(){
+		      var $this = $(this);
+		      if($this.val() == ''){
+		          $this.val('Compose a new post...');
+		      }
+		  });
+		</script>
+	</html>
+<?php
+}
 ?>
-<html>
-	<head>
-		<title>Microblogging site</title>
-		<link href='http://fonts.googleapis.com/css?family=Signika:400,300,700,600' rel='stylesheet' type='text/css'>
-		<link href='css/main.css' rel='stylesheet' type='text/css'>
-	</head>
-	<body>
-		<div id="headerWrapper">
-		</div>
-		<div id="contentWrapper">
-			<div id="sidebarWrapper" class="curvedBorder">
-				<?php loadModule('profileImage'); ?>
-				<?php loadModule('followCount'); ?>
-				<?php loadModule('newPost'); ?>
-			</div>
-			<div id="timelineWrapper" class="curvedBorder">
-				<?php timelinePosts(NULL); ?>
-			</div>
-		</div>
-	</body>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-	<script>
-	$('textarea.newPost')
-	  .on('focus', function(){
-	      var $this = $(this);
-	      if($this.val() == 'Compose a new post...'){
-	          $this.val('');
-	      }
-	  })
-	  .on('blur', function(){
-	      var $this = $(this);
-	      if($this.val() == ''){
-	          $this.val('Compose a new post...');
-	      }
-	  });
-	</script>
-</html>

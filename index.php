0b66b21bc4a10d9bd43c79603e2aebc0f0199f59
Tyler Bridger <tyler@192-168-20-150.tpgi.com.au>
2013-12-17 15:23:16 +0900

    Added main structure to homepage

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-This is the homepage.
+<html>
+	<head>
+		<title>Microblogging site</title>
+	</head>
+	<body>
+		<div id="contentWrapper">
+			<div id="timelineWrapper">
+				
+			</div>
+			<div id="sidebarWrapper">
+				
+			</div>
+		</div>
+	</body>
+</html>

c00cf7dbcc8a69c992cad567eb46a3ac045b6625
Tyler Bridger <tyler@192-168-20-150.tpgi.com.au>
2013-12-17 16:01:15 +0900

    Added sample code

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
 <html>
 	<head>
 		<title>Microblogging site</title>
-		<link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Signika:400,300,700,600' rel='stylesheet' type='text/css'>
 		<link href='css/main.css' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
+		<div id="headerWrapper">
+		</div>
 		<div id="contentWrapper">
-			<div id="timelineWrapper">
-				
-			</div>
 			<div id="sidebarWrapper">
 				
+			</div>
+			<div id="timelineWrapper">
+				<div class="timelineEntry">
+					This is a text post.
+				</div>
+				<div class="timelineEntry">
+					<div class="imageWrapper">
+						<img src="https://pbs.twimg.com/profile_images/378800000281645191/148c9aa0fb90a3d0a422e28a66786db5.jpeg">
+					</div>
+				</div>
 			</div>
 		</div>
 	</body>

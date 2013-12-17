3f7c4b2da34d05036b2a19611c09c929f46a41ce
Tyler Bridger <tyler@192-168-20-150.tpgi.com.au>
2013-12-17 16:01:45 +0900

    Added sample code

--- index.php
+++ index.php
@@ -12,13 +12,9 @@
 				
 			</div>
 			<div id="timelineWrapper">
+				<div class="timelineEntry">This is a text post.</div>
 				<div class="timelineEntry">
-					This is a text post.
-				</div>
-				<div class="timelineEntry">
-					<div class="imageWrapper">
-						<img src="https://pbs.twimg.com/profile_images/378800000281645191/148c9aa0fb90a3d0a422e28a66786db5.jpeg">
-					</div>
+					<div class="imageWrapper"><img src="https://pbs.twimg.com/profile_images/378800000281645191/148c9aa0fb90a3d0a422e28a66786db5.jpeg"></div>
 				</div>
 			</div>
 		</div>

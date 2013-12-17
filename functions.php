b44fc4dc9f2ef0551b98a19b4a5a60e647afb78e
Tyler <tylerbridger96@gmail.com>
2013-12-17 18:16:15 +0900

    Added session based profile images

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 function loadModule($switch){
 	switch ($switch){
 		case 'profileImage':
-			echo '<div id="profileImage" class="sidebarModule"><img src="https://pbs.twimg.com/profile_images/378800000281645191/148c9aa0fb90a3d0a422e28a66786db5.jpeg" class="image"></div>';
+			echo '<div id="profileImage" class="sidebarModule"><img src="/media/image/' . $_SESSION['user'] . '" class="image"></div>';
 			break;
 		case 'followCount':
 			echo '<div id="followCount" class="sidebarModule">Followers: 100<br>Following: 200</div>';
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$_SESSION['user'] = 'tyler';
 require 'functions.php';
 require 'sql.php';
 ?>

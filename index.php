7088db90ae1b570a886a7ea4286cf211c1beab20
Tyler <tylerbridger96@gmail.com>
2013-12-17 17:17:16 +0900

    Fixed profile image and removed sql.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 			<div id="timelineWrapper">
 				<div class="timelineEntry">This is a text post.</div>
 				<div class="timelineEntry">
-					<div class="imageWrapper"><img src="https://pbs.twimg.com/profile_images/378800000281645191/148c9aa0fb90a3d0a422e28a66786db5.jpeg"></div>
+					<div class="imageWrapper"><img src="https://pbs.twimg.com/profile_images/378800000281645191/148c9aa0fb90a3d0a422e28a66786db5.jpeg" class="image"></div>
 				</div>
 			</div>
 		</div>
